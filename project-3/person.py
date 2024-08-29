d7004441375a38731d33cfc58286c6b4e0ef3b0a
Parsam <1384parsamnorouzi@gmail.com>
2024-08-29 15:32:27 -0400

    Add files via upload

---
+++ project-3/person.py
@@ -0,0 +1,13 @@
+firstName = input('please give me your first name: ')
+
+lastName = input('plaese give me last name: ')
+
+age = input('please give me your age: ')
+
+print(firstName + " " + lastName + ' is ' + age)
+
+afterYear = input('say a number betwwen 1 to 100: ')
+
+sumAge = int(age) + int(afterYear)
+
+print('I will be ' + str(sumAge) + ' years old after ' + str(int(afterYear)) + ' years.')
