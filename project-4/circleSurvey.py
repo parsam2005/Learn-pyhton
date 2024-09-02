8172e64c64260c7571852835d4ddaed6d4482f5b
Parsam <1384parsamnorouzi@gmail.com>
2024-09-02 13:54:46 -0400

    Survey

---
+++ project-4/circleSurvey.py
@@ -0,0 +1,4 @@
+radius  = input('enter the desired redius: ')
+pi = 3.14
+circleSurvey = pi * int(radius) ** 2
+print('The Survey is :' , circleSurvey , ', cm^2')
---
+++ project-4/squareSurvey.py
@@ -0,0 +1,4 @@
+length = input('enter the desired length: ')
+width = input('enter the desired width: ')
+squareSurvey = int(length) * int(width)
+print("The survey is :" , squareSurvey , "m^2")
